205518c5854b3438dba37e2f59615c51e337dede
James R. Qualls <Qualls.James@gmail.com>
2014-01-23 18:02:10 -0500

    home page rec > 1

--- controllers/index.js
+++ controllers/index.js
@@ -18,15 +18,18 @@ module.exports = {
                 'a.secure_url,'+
                 'pi.item_name,'+
                 'pi.brand_name,'+
-                'pi.id as item_id '+
+                'pi.id as item_id,'+
+                'count(*) as like_count '+
             'from predict_training p '+
             'join assets a on a.upc = p.upc '+
             'join predict_items pi on pi.upc = p.upc '+
+            'where p.gender = "male" '+
             'and a.tag_id = 1 '+
             'and p.liked = 1 '+
             'and a.deleted != 1 '+
             'and a.watermarked = 1 '+
-            'order by RAND() '+
+            'group by p.upc '+
+            'having like_count > 1 '+
             'limit 6'
         ).exec(function (err, data){
             
@@ -34,7 +37,7 @@ module.exports = {
                 return next(err);
             }
 
-            console.log(data[0]);
+            // console.log(data[0]);
             res.render('index',{
                 title: 'Home',
                 messages: req.flash('messages'),
