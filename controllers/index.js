b3cc6dd5182e1bc7fde4e60d759d618c19be48d5
James R. Qualls <Qualls.James@gmail.com>
2014-01-23 17:41:23 -0500

    added recently liked

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,7 @@
 /** 
  * Login Controller 
  */
+var knex = require('knex').knex;
 
 module.exports = {
     index: function (req, res, next) {
@@ -9,9 +10,41 @@ module.exports = {
             return res.redirect('/item')
         }
 
-        res.render('index',{
-            title: 'Home',
-            messages: req.flash('messages')
+
+
+        knex.raw(
+            'select '+
+                'p.*,'+
+                'a.secure_url,'+
+                'pi.item_name,'+
+                'pi.brand_name,'+
+                'pi.id as item_id '+
+            'from predict_training p '+
+            'join assets a on a.upc = p.upc '+
+            'join predict_items pi on pi.upc = p.upc '+
+            'and a.tag_id = 1 '+
+            'and p.liked = 1 '+
+            'and a.deleted != 1 '+
+            'and a.watermarked = 1 '+
+            'order by RAND() '+
+            'limit 6'
+        ).exec(function (err, data){
+            
+            if (err) {
+                return next(err);
+            }
+
+            console.log(data[0]);
+            res.render('index',{
+                title: 'Home',
+                messages: req.flash('messages'),
+                items: data[0]
+            });    
+
         });
+
+
+
+        
     }
 };
